3c48bd992337d56e92718abc77b3d263825ca317
talha789 <talha.amjad@techandover.com>
2022-02-09 18:50:22 +0500

    t and c and dis

--- src/views/crafted/authentication/basic-flow/disclaimer.vue
+++ src/views/crafted/authentication/basic-flow/disclaimer.vue
@@ -3,17 +3,44 @@
   <div
     style="width: 1000px !important"
     class="w-lg-500px bg-white rounded shadow-sm p-10 p-lg-15 mx-auto"
+    v-html="inner"
   ></div>
   <!--end::Wrapper-->
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, onMounted, ref } from "vue";
+import { useStore } from "vuex";
+import { Actions } from "@/store/enums/StoreEnums";
+import Quill from "quill";
 
 export default defineComponent({
   name: "disclaimer",
   setup() {
-    return {};
+    const store = useStore();
+    let termsAndConditions = { value: "" };
+    const inner = ref<string>("");
+    const quill = new Quill();
+    console.log({ quill });
+    onMounted(async () => {
+      try {
+        const response = await store.dispatch(
+          Actions.GET_CONFIGURATION_BY_KEY,
+          "disclaimer"
+        );
+        if (!response.success) throw new Error();
+        termsAndConditions = response.data;
+        let article = document.createElement("article");
+        let quill = new Quill(article, { readOny: true });
+        quill.setContents(JSON.parse(termsAndConditions.value));
+        inner.value = quill.root.innerHTML;
+      } catch (err) {
+        console.log(err);
+      }
+    });
+    return {
+      inner,
+    };
   },
 });
 </script>
